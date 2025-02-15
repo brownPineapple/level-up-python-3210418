ab13a895eec19c0ec707560f4de99188731091a2
brownPineapple <30342446+brownPineapple@users.noreply.github.com>
2025-02-15 14:08:56 +0000

    palindrome program - final

--- my_files/palindrome.py
+++ my_files/palindrome.py
@@ -2,7 +2,7 @@ def is_palindrome(string1):
   string1 = ''.join(e for e in string1 if e.isalnum()).lower() #Remove special characters and white spaces
   string2 = string1[::-1] #Reverse the string
 #  print(f'String1 - {string1} and reversed looks like - {string2}') #Uncomment to see behind the scenes
-  return True if string1 == string2 else False
+  return string1 == string2
 
 print("*"*8 + " Palindrome checker! " + "*"*8) #Banner 
 user_string = input("Enter a string: ") #Get a string from user

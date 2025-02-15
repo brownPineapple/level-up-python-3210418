963612d78a107efc7e12214707d5630a576e8687
brownPineapple <30342446+brownPineapple@users.noreply.github.com>
2025-02-15 13:46:48 +0000

    prime factor calculation final

--- my_files/prime_factors.py
+++ my_files/prime_factors.py
@@ -1,16 +1,16 @@
 print('Welcome to Prime Factor calculator!')
 
-num = int(input("Enter a number: "))
-
 def prime_factors(n):
-  factors = []
-  divisor = 2
-  while divisor <= n:
+  factors = [] # Create an empty list for factors
+  divisor = 2 # init divisor
+  while divisor <= n: 
     if n % divisor == 0:
-      factors.append(divisor)
-      n = n // divisor
+      factors.append(divisor) # if no remainder append factor list
+      n = n // divisor # set n to the new value
     else:
-      divisor += 1
+      divisor += 1 # increment divisor
   return factors
+
+num = int(input("Enter a number: "))
 
 print(prime_factors(num))
